03a00621a634477b9954a46b5b0112f1ddf0818f
Inkvii <22601194+Inkvii@users.noreply.github.com>
2024-01-27 08:37:57 +0100

    initial commit

--- src/library/table/utils/cellDisplay.ts
+++
@@ -1,5 +0,0 @@
-import { CellContext } from "@tanstack/react-table"
-
-export function getValueOrDefault<TData, TValue>(context: CellContext<TData, TValue>, defaultValue: string = "[null]") {
-  return context.getValue() ?? defaultValue
-}
